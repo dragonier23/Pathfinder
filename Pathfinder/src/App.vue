7087559a639115382f369738b6f0a0b3487f2574
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2023-06-28 17:26:42 +0800

    Updated grid, and implemented start and end nodes

--- Pathfinder/src/App.vue
+++ Pathfinder/src/App.vue
@@ -1,30 +1,112 @@
 <script setup>
-    import Node from './components/Node.vue'
-    import { ref } from 'vue'
+import Node from './components/Node.vue'
+import { ref } from 'vue'
 
-    const widthCount = (window.innerWidth / 40) - 2;
-    const heightCount = (window.innerHeight / 40) - 1;
+const verticalCount = window.innerHeight / 40 - 3
+const horizontalCount = 12 //window.innerWidth / 40;
 
+//initiatilize some sort of control over the start and end
+const startRow = ref(2)
+const startColumn = ref(3)
+const endRow = ref(13)
+const endColumn = ref(horizontalCount - 4)
 
-    const gridArray = [];
-    for (let row = 0; row < heightCount; row++){
-        const currentRow = [];
-        for (let column = 0; column < widthCount; column++){
-            currentRow.push([]);
-        }
-        gridArray.push(currentRow);
+//help decide if is moving start or moving end
+const moveStart = ref(false)
+const moveEnd = ref(false)
+
+//create grid
+const grid = []
+for (var row = 0; row < verticalCount; row++) {
+  const rowGrid = []
+  for (var column = 0; column < horizontalCount; column++) {
+    rowGrid.push([])
+  }
+  grid.push(rowGrid)
+}
+
+//so, first on mouse down: 1. this will trigger a function, that first decides:
+// are we moving a start or end node - emitted event from child Node --> send if isStartNode or isEndNode is true + row and column number
+// next it decides: are we setting it down, or picking it up
+
+//so if picking up, we try to locate where the mouse is by listening to the hover event over the particular node, which does the same thing as the click event except it just keeps running
+
+//if putting down, we take the click event, and use the emitted row and column thing to set the startRow/endRow to the newly chosen thing
+
+//triggers the moving process
+function nodeClicked(isStartNode, isEndNode, row, column) {
+  if (moveEnd.value) {
+    moveEnd.value = !moveEnd.value
+    endRow.value = row
+    endColumn.value = column
+  } else if (!moveEnd.value) {
+    if (isEndNode) {
+      moveEnd.value = !moveEnd.value
     }
-    const grid = ref(gridArray);
+  } else if (moveStart.value) {
+    moveStart.value = !moveStart.value
+    startRow.value = row
+    startColumn.value = column
+  } else if (!moveStart.value) {
+    if (isStartNode) {
+      moveStart.value = !moveStart.value
+    }
+  }
+}
+
+//during the moving process, if the mouse is hovering over a particular node, shade it the color of the currently moved node (green or red)
+function nodeHover(row, column) {
+  if (moveStart.value) {
+    startRow.value = row
+    startColumn.value = column
+  } else if (moveEnd.value) {
+    endRow.value = row
+    endColumn.value = column
+  }
+}
 </script>
 
 <template>
-    <div v-for="(row, rowIndex) of grid" class="row">
-        <Node v-for="(column, columnIndex) of row" :row="rowIndex" :column="columnIndex"/>
+  <div class="centered">
+    <div v-for="(row, rowIndex) in grid" :row="rowIndex" class="row">
+      <template v-for="(column, columnIndex) in row">
+        <Node
+          v-if="rowIndex === startRow && columnIndex === startColumn"
+          :row="rowIndex"
+          :column="columnIndex"
+          :isStartNode="true"
+          :isEndNode="false"
+          @nodeClick="nodeClicked"
+          @nodeHover="nodeHover"
+        />
+        <Node
+          v-else-if="rowIndex === endRow && columnIndex === endColumn"
+          :row="rowIndex"
+          :column="columnIndex"
+          :isStartNode="false"
+          :isEndNode="true"
+          @nodeClick="nodeClicked"
+          @nodeHover="nodeHover"
+        />
+        <Node
+          v-else
+          :row="rowIndex"
+          :column="columnIndex"
+          :isStartNode="false"
+          :isEndNode="false"
+          @nodeClick="nodeClicked"
+          @nodeHover="nodeHover"
+        />
+      </template>
     </div>
+  </div>
 </template>
 
-<style scoped>
-.row{
-    margin-top: 0px;
+<style>
+.row {
+  height: 40px;
+}
+.centered {
+  margin: auto;
 }
 </style>
--- pathfinder/src/components/Node.vue
+++ pathfinder/src/components/Node.vue
@@ -2,23 +2,47 @@
 import { ref } from 'vue'
 
 const squareClass = ref('square')
+
+const emit = defineEmits(['nodeClick', 'nodeHover'])
+
 const props = defineProps({
   row: Number,
-  column: Number
+  column: Number,
+  isStartNode: Boolean,
+  isEndNode: Boolean
 })
+
+//sends back whether it is the start or end node, and sends back the row and column associated with the node
+const clickResponse = () => {
+  emit('nodeClick', props.isStartNode, props.isEndNode, props.row, props.column)
+}
+
+const hoverResponse = () => {
+  emit('nodeHover', props.row, props.column)
+}
 </script>
 
 <template>
-  <div :class="squareClass" row>
-  
-  </div>
+  <div
+    :class="[squareClass, { startNode: isStartNode }, { endNode: isEndNode }]"
+    @click="clickResponse"
+    @mouseover="hoverResponse"
+  ></div>
 </template>
 
-<style scoped>
+<style>
 .square {
-  width: 40px;
   height: 40px;
-  outline: 1px solid grey;
+  width: 40px;
   display: inline-block;
+  border: 1px solid grey;
+}
+
+.startNode {
+  background-color: green;
+}
+
+.endNode {
+  background-color: red;
 }
 </style>
